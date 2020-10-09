db45e1d34b04d44dcd499dc06c80e4e242c805e3
machula.remigiusz <machula.remigiusz@gmail.com>
2020-10-09 23:29:28 +0200

    feat: add confirm reservation

--- src/Controller/ReservationController.php
+++ src/Controller/ReservationController.php
@@ -5,6 +5,10 @@ namespace App\Controller;
 use App\Entity\Reservation;
 use App\Form\ReservationType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\Flash\FlashBagInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
@@ -13,8 +17,12 @@ use Symfony\Component\Routing\Annotation\Route;
 class ReservationController extends AbstractController
 {
 
-    public function __construct()
+    /** @var FlashBagInterface */
+    private $flashBag;
+
+    public function __construct(FlashBagInterface $flashBag)
     {
+        $this->flashBag = $flashBag;
     }
 
     /**
@@ -32,5 +40,58 @@ class ReservationController extends AbstractController
             'reservationForm' => $form->createView(),
         ];
         return $this->render('makeReservation/reservation.html.twig', $viewData);
+    }
+
+    /**
+     * @Route("/confirm", name="confirm")
+     * @param Request $request
+     * @return RedirectResponse|Response
+     */
+    public function confirmReservation(Request $request)
+    {
+        $reservation = new Reservation();
+        $form = $this->createForm(ReservationType::class, $reservation);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            /** @var Reservation $reservationToConfirm */
+            $reservationToConfirm = $form->getData();
+            if ($this->isReservationPossibleToMake($reservationToConfirm)) {
+
+                $reservationToConfirm->setTotalPrice($reservationToConfirm->calculatePrice());
+
+                $viewData = [
+                    'reservation' => $reservationToConfirm,
+                ];
+                return $this->render('makeReservation/reservation-confirm.html.twig', $viewData);
+            }
+
+        }
+        $this->flashBag->add('danger', 'Something went wrong');
+        return $this->redirectToRoute('reservation_index');
+    }
+
+    /**
+     * @param Reservation $reservationToConfirm
+     * @return bool
+     */
+    private function isReservationPossibleToMake(Reservation $reservationToConfirm)
+    {
+        $reservations = $reservationToConfirm->getHotel()->getReservations();
+
+        $from = new \DateTime();
+        $from->setTimestamp($reservationToConfirm->getStartDate()->getTimestamp());
+        $to = $reservationToConfirm->getEndDate();
+        while ($from != $to) {
+            if (!key_exists($from->format('yyyy-MM-dd'), (array)$reservations)) {
+                $reservations[$from->format('yyyy-MM-dd')] = 0;
+            }
+
+            if ($reservationToConfirm->getBeds() > $reservationToConfirm->getHotel()->getBeds() - $reservations[$from->format('yyyy-MM-dd')]) {
+                return false;
+            }
+            $from->modify("+1 day");
+        }
+        return true;
     }
 }
