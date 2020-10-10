28ec837ab1ea64b1dc8f7b2d93a19934c4a61a23
machula.remigiusz <machula.remigiusz@gmail.com>
2020-10-10 09:33:40 +0200

    feat: Add cancel reservation functionality

--- src/Controller/CancelReservationController.php
+++ src/Controller/CancelReservationController.php
@@ -2,10 +2,10 @@
 
 namespace App\Controller;
 
-use App\Entity\Reservations;
+use App\Entity\Reservation;
 use App\Form\CancelReservationType;
 use App\Form\ReservationType;
-use App\Repository\ReservationsRepository;
+use App\Repository\ReservationRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -18,14 +18,20 @@ use Symfony\Component\Routing\Annotation\Route;
  */
 class CancelReservationController extends AbstractController
 {
+    /** @var ReservationRepository */
+    private $reservationsRepository;
+
     /** @var FlashBagInterface */
     private $flashBag;
 
     /**
+     * @param ReservationRepository $reservationsRepository
      * @param FlashBagInterface $flashBag
      */
-    public function __construct(FlashBagInterface $flashBag)
+    public function __construct(ReservationRepository $reservationsRepository,
+                                 FlashBagInterface $flashBag)
     {
+        $this->reservationsRepository = $reservationsRepository;
         $this->flashBag = $flashBag;
     }
 
@@ -54,6 +60,70 @@ class CancelReservationController extends AbstractController
      */
     public function isReservationPossibleToCancel(Request $request)
     {
-        return $this->render('cancelReservation/reservation-cancel-confirm.html.twig');
+        $formData = [
+            'reservationNumber' => null,
+        ];
+        $form = $this->createForm(CancelReservationType::class, $formData);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $reservationNumber = $form->getData()['reservationNumber'];
+            $reservation = $this->reservationsRepository->find($reservationNumber) ;
+            $today = new \DateTime();
+            if (/*$reservation->getStartDate()->getTimestamp() > $today->getTimestamp() && */ $reservation->getStartDate()->diff($today)->days > 2) {
+                $viewData = [
+                    'reservation' => $reservation,
+                ];
+                return $this->render('cancelReservation/reservation-cancel-confirm.html.twig', $viewData);
+            }
+        }
+        $this->flashBag->add('danger', 'Too late to cancel');
+        return $this->redirectToRoute('reservation_cancel');
+    }
+
+    /**
+     * @Route("/confirmed", name="cancel_confirmed")
+     * @param Request $request
+     * @return RedirectResponse
+     */
+    public function cancelReservationConfirmed(Request $request)
+    {
+        $reservation = new Reservation();
+        $form = $this->createForm(ReservationType::class, $reservation);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $reservation = $form->getData();
+            $this->deleteReservation($reservation);
+
+            $this->flashBag->add('success', 'reservation cancelled!');
+        } else {
+            $this->flashBag->add('danger', 'Something went wrong');
+        }
+
+        return $this->redirectToRoute('cancel_reservation');
+    }
+
+    private function deleteReservation(Reservation $reservation)
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+
+        $hotel = $reservation->getHotel();
+
+        $hotelReservations = $hotel->getReservations();
+        $from = new \DateTime();
+        $from->setTimestamp($reservation->getStartDate()->getTimestamp());
+        $to = $reservation->getEndDate();
+        while ($from != $to) {
+            $hotelReservations[$from->format('yyyy-MM-dd')]-= $reservation->getBeds() ;
+            if ($hotelReservations[$from->format('yyyy-MM-dd')] == 0) {
+                unset($hotelReservations[$from->format('yyyy-MM-dd')]);
+            }
+            $from = $from->modify("+1 day");
+        }
+        $hotel->setReservations($hotelReservations);
+
+        $entityManager->persist($hotel);
+        $entityManager->remove($reservation);
+        $entityManager->flush();
     }
 }
